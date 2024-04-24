7592c91804a1b84b2c9a1e1224f48a4f0ab5d8e8
j-0-n-e-z <j000neeez@gmail.com>
2024-04-24 14:28:53 +0500

    :tada: add simple router

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ import http from 'http'
 const PORT = process.env.PORT
 
 const server = http.createServer((req, res) => {
-	// res.setHeader('Content-Type', 'text/html') // text/plain, text/html, application/json
-  // res.statusCode = 404
-  // Shorthand for what's above:
-	res.writeHead(200, { 'Content-Type': 'text/html' })
-
-	res.end('<blockquote>Hello&nbsp;World</blockquote>')
+	try {
+		if (req.method === 'GET') {
+			if (req.url === '/') {
+				res.writeHead(200, { 'Content-Type': 'text/html' })
+				res.end('<h1>Homepage</h1>')
+			} else if (req.url === '/about') {
+				res.writeHead(200, { 'Content-Type': 'text/html' })
+				res.end('<h1>About</h1>')
+			} else {
+				res.writeHead(404, { 'Content-Type': 'text/html' })
+				res.end('<h1>Not found</h1>')
+			}
+		} else {
+			throw new Error('Method not allowed')
+		}
+	} catch (error) {
+		res.writeHead(500, { 'Content-Type': 'text/plain' })
+		res.end('Server Error')
+	}
 })
 
 server.listen(PORT, () => {
