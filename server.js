6a2d430f244d83ad5a24f9d671c3cbf14ba7d520
j-0-n-e-z <j000neeez@gmail.com>
2024-04-24 14:46:36 +0500

    :tada: add sending html with router

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
+import fs from 'fs/promises'
 import http from 'http'
+import path from 'path'
+import url from 'url'
 
 const PORT = process.env.PORT
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url) // path to server.js
+const __dirname = path.dirname(__filename) // path to folder with server.js inside
+
+const server = http.createServer(async (req, res) => {
 	try {
 		if (req.method === 'GET') {
+			let filePath
+
 			if (req.url === '/') {
-				res.writeHead(200, { 'Content-Type': 'text/html' })
-				res.end('<h1>Homepage</h1>')
+				filePath = path.join(__dirname, 'public', 'index.html')
 			} else if (req.url === '/about') {
-				res.writeHead(200, { 'Content-Type': 'text/html' })
-				res.end('<h1>About</h1>')
+				filePath = path.join(__dirname, 'public', 'about.html')
 			} else {
-				res.writeHead(404, { 'Content-Type': 'text/html' })
-				res.end('<h1>Not found</h1>')
+				filePath = path.join(__dirname, 'public', '404.html')
 			}
+
+			const data = await fs.readFile(filePath)
+			res.writeHead(200, 'Content-Type', 'text/html')
+			res.end(data)
 		} else {
 			throw new Error('Method not allowed')
 		}
 	} catch (error) {
 		res.writeHead(500, { 'Content-Type': 'text/plain' })
-		res.end('Server Error')
+		res.end(error.message)
 	}
 })
 
