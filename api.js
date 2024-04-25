7826077cc5420c7eeef36a6a90a9526861973c1f
j-0-n-e-z <j000neeez@gmail.com>
2024-04-25 12:56:20 +0500

    :tada: add some middlewares

--- api.js
+++ api.js
@@ -8,23 +8,53 @@ const users = [
 	{ id: 3, name: 'Jim Doe' }
 ]
 
-const server = createServer((req, res) => {
+// Logger middleware
+const logger = (req, res, next) => {
+	console.log(`${req.method} ${req.url}`)
+	next()
+}
+
+// JSON middleware
+const jsonMiddleware = (req, res, next) => {
 	res.setHeader('Content-Type', 'application/json')
-	if (req.url === '/api/users' && req.method === 'GET') {
-		res.end(JSON.stringify(users))
-	} else if (req.url.match(/\/api\/users\/\d+/) && req.method === 'GET') {
-		const id = req.url.split('/')[3]
-		const user = users.find(user => user.id == id)
-		if (user) {
-			res.end(JSON.stringify(user))
-		} else {
-			res.statusCode = 404
-			res.end(JSON.stringify({ message: 'User not found' }))
-		}
+	next()
+}
+
+// Route handler for GET /api/users
+const getUsersHandler = (req, res) => {
+	res.end(JSON.stringify(users))
+}
+
+// Route handler for GET /api/users/:id
+const getUserByIdHandler = (req, res) => {
+	const id = req.url.split('/')[3]
+	const user = users.find(user => user.id == id)
+	if (user) {
+		res.end(JSON.stringify(user))
 	} else {
 		res.statusCode = 404
-		res.end(JSON.stringify({ message: 'Route not found' }))
+		res.end(JSON.stringify({ message: 'User not found' }))
 	}
+}
+
+// Not found handler
+const notFoundHandler = (req, res) => {
+	res.statusCode = 404
+	res.end(JSON.stringify({ message: 'Route not found' }))
+}
+
+const server = createServer((req, res) => {
+	logger(req, res, () => {
+		jsonMiddleware(req, res, () => {
+			if (req.url === '/api/users' && req.method === 'GET') {
+				getUsersHandler(req, res)
+			} else if (req.url.match(/\/api\/users\/\d+/) && req.method === 'GET') {
+				getUserByIdHandler(req, res)
+			} else {
+				notFoundHandler(req, res)
+			}
+		})
+	})
 })
 
 server.listen(PORT, () => {
